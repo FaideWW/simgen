92e03dec9a53aaffa56444e547f4cf8a05a83bd0
Michael Ko <michael@faide.net>
2017-06-28 17:45:21 -0600

    Shrink input files

--- makeAll.py
+++ makeAll.py
@@ -1,5 +1,5 @@
 import os
 
-os.system('python runSims.py trinket 940 simc.exe -c 50 --nosim')
-os.system('python runSims.py trinket 920 simc.exe -c 50 --nosim')
-os.system('python runSims.py trinket 900 simc.exe -c 50 --nosim')
+os.system('python runSims.py trinket 940 simc.exe -c 100 --nosim')
+os.system('python runSims.py trinket 920 simc.exe -c 100 --nosim')
+os.system('python runSims.py trinket 900 simc.exe -c 100 --nosim')
